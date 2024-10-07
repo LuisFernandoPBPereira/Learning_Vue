7bade0ec2e7700a681d874a0741ec5c34685990a
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-10-07 15:47:51 -0300

    feat: Criando barra lateral e home

---
+++ AluraTracker/src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div>
+    <div class="column is-three-quarter">
+      <Formulario/>
+    </div>
+  </main>
+</template>
+
+<script>
+import BarraLateral from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue'
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components:{
+    BarraLateral,
+    Formulario
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ AluraTracker/src/components/BarraLateral.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>
+            <img src="https://raw.githubusercontent.com/alura-cursos/alura-tracker/main/src/assets/logo.png" alt="Fotinha">
+        </h1>
+    </header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+header{
+    padding: 1rem;
+    background-color: #0d3b66;
+    width: 100%;
+    height: 100vh;
+}
+
+@media only screen and (max-width: 768px){
+    header{
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
