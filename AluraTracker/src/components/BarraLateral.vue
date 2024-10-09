aa14e6b1ca5475c6280f46ba73a214007e5155c3
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-10-09 16:40:18 -0300

    feat: Adicionando botões para as rotas

--- AluraTracker/src/components/BarraLateral.vue
+++ AluraTracker/src/components/BarraLateral.vue
@@ -3,9 +3,27 @@
         <h1>
             <img src="https://raw.githubusercontent.com/alura-cursos/alura-tracker/main/src/assets/logo.png" alt="Fotinha">
         </h1>
-        <button class="button" @click="alterarTema">
-            {{textoDoBotao}}
-        </button>
+        <div class="has-text-centered">
+            <button class="button" @click="alterarTema">
+                {{ textoDoBotao }}
+            </button>
+        </div>
+        <nav class="panel mt-5">
+            <ul>
+                <li>
+                    <RouterLink to="/" class="link">
+                        <i class="fas fa-tasks"></i>
+                        Tarefas
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/projetos" class="link">
+                        <i class="fas fa-project-diagram"></i>
+                        Projetos
+                    </RouterLink>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -15,31 +33,31 @@ import { defineComponent } from 'vue';
 export default defineComponent({
     name: 'BarraLateral',
     emits: ['aoAlterarTema'],
-    data(){
-        return{
+    data() {
+        return {
             modoEscuroAtivo: false
         }
     },
-    computed:{
-        textoDoBotao(){
-            if(this.modoEscuroAtivo){
+    computed: {
+        textoDoBotao() {
+            if (this.modoEscuroAtivo) {
                 return "Desativar modo escuro"
             }
 
             return "Ativar modo escuro"
         }
     },
-    methods:{
-        alterarTema(){
+    methods: {
+        alterarTema() {
             this.modoEscuroAtivo = !this.modoEscuroAtivo,
-            this.$emit('aoAlterarTema', this.modoEscuroAtivo)
+                this.$emit('aoAlterarTema', this.modoEscuroAtivo)
         }
     }
 })
 </script>
 
 <style scoped>
-header{
+header {
     padding: 1rem;
     background-color: #0d3b66;
     width: 100%;
@@ -47,10 +65,26 @@ header{
     text-align: center;
 }
 
-@media only screen and (max-width: 768px){
-    header{
+@media only screen and (max-width: 768px) {
+    header {
         padding: 2.5rem;
         height: auto;
     }
+}
+
+.panel li{
+    margin: 8px 0;
+}
+
+.link{
+    color: #fff;
+}
+
+.link:hover{
+    color:  #FAF0CA;
+}
+
+.link.router-link-active{
+    color:  #FAF0CA;
 }
 </style>
